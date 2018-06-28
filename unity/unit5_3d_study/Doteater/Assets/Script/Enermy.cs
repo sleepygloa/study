8d8ad1860278c703fd7d91573340742f5394207d
sleepygloa <sleepygloa@gmail.com>
2018-06-28 15:57:04 +0900

    backup

---
+++ unity/unit5_3d_study/Doteater/Assets/Script/Enermy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enermy : MonoBehaviour {
+
+    public GameObject target;
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.destination = target.transform.position;
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+	}
+}
