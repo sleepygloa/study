5962707c496848fc88c77d9400c88206a23a2e52
sleepygloa <sleepygloa@gmail.com>
2018-07-11 23:07:30 +0900

    수정) 레이아웃 뷰 소스 수정

---
+++ android_study/SampleLogin/app/src/main/java/com/seonhoblog/samplelogin/Layout/FrameViewActivity.java
@@ -0,0 +1,16 @@
+package com.seonhoblog.samplelogin.Layout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.seonhoblog.samplelogin.R;
+
+public class FrameViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_frameview);
+    }
+}
--- android_study/SampleLogin/app/src/main/java/com/seonhoblog/samplelogin/Layout/LayoutActivity.java
+++ android_study/SampleLogin/app/src/main/java/com/seonhoblog/samplelogin/Layout/LayoutActivity.java
@@ -1,18 +1,80 @@
 package com.seonhoblog.samplelogin.Layout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.seonhoblog.samplelogin.MainActivity;
 import com.seonhoblog.samplelogin.R;
 
+
 public class LayoutActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout);
 
+        Button buttonToMain = (Button) findViewById(R.id.btn_layout_tomain);
+        Button buttonLinear = (Button) findViewById(R.id.btn_layout_linear);
+        Button buttonRelative = (Button) findViewById(R.id.btn_layout_relative);
+        Button buttonGrid = (Button) findViewById(R.id.btn_layout_grid);
+        Button buttonFrame = (Button) findViewById(R.id.btn_layout_frame);
+        Button bottonTotal = (Button) findViewById(R.id.btn_layout_total);
+        Button buttonContraint = (Button) findViewById(R.id.btn_layout_contraint);
 
-
+        buttonToMain.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                toPage(MainActivity.class);
+            }
+        });
+        buttonLinear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                toPage(MainActivity.class);
+            }
+        });
+        buttonRelative.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                toPage(MainActivity.class);
+            }
+        });
+        buttonGrid.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                toPage(MainActivity.class);
+            }
+        });
+        buttonFrame.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                toPage(MainActivity.class);
+            }
+        });
+        bottonTotal.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                toPage(TotalViewActivity.class);
+            }
+        });
+        buttonContraint.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                toPage(ContraintViewActivity.class);
+            }
+        });
     }
+
+    private void toPage(Class activity){
+        Intent intent = new Intent(getApplicationContext(), activity);
+        startActivity(intent);
+    }
+
+
+
 }
