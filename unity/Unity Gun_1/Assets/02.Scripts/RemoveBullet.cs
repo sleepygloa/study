72ffd5f87193d8e3bffb2beb2dd96f0c11e23992
sleepygloa <sleepygloa@gmail.com>
2019-01-04 00:15:23 +0900

    unity_gun1 impliments gun effect

---
+++ unity/Unity Gun_1/Assets/02.Scripts/RemoveBullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    //스파크 프리팹을 저장할 변수
+    public GameObject sparkEffect;
+
+    //충돌이 시작할 때 발생하는 이벤트
+    private void OnCollisionEnter(Collision collision)
+    {
+        //충돌한 게임오브젝트의 태그값 비교
+        if(collision.collider.tag == "BULLET") 
+        {
+            //스파크 효과 함수 호출
+            ShowEffect(collision);
+            //충돌한 게임오브젝트 삭제
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void ShowEffect(Collision coll) 
+    {
+        //충돌 지점의 정보를 추출
+        ContactPoint contact = coll.contacts[0];
+
+        //법선 벡터가 이루는 회전각도를 추출
+        Quaternion rot = Quaternion.FromToRotation(-Vector3.forward, contact.normal);
+
+        //스파크 효과를 생성
+        Instantiate(sparkEffect, contact.point, rot);
+     }
+}
