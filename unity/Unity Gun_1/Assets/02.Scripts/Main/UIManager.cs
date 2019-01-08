f6553486c84626c8dc850d4ef83d354f797769f7
sleepygloa <sleepygloa@gmail.com>
2019-01-09 01:01:56 +0900

    unity_gun1 impliments UI

---
+++ unity/Unity Gun_1/Assets/02.Scripts/Main/UIManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public void OnClickStartBtn(string msg)
+    {
+        Debug.Log("Click Button" + msg);
+    }
+}
--- unity/Unity Gun_1/Assets/02.Scripts/Player/FireCtrl.cs
+++ unity/Unity Gun_1/Assets/02.Scripts/Player/FireCtrl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //총알 발사와 재장전 오디오 클립을 저장할 구조체
 [System.Serializable]
@@ -44,6 +45,22 @@ public class FireCtrl : MonoBehaviour
     //Shake 클래스를 저장할 변수
     //public Shake shake;
 
+    //탄창 이미지 Image Ui
+    public Image magazineImg;
+    //남은 총알 수 Text UI
+    public Text magazineText;
+
+    //최대 총알 수
+    public int maxBullet = 10;
+    //남은 총알 수
+    public int remainingBullet = 10;
+
+    //재장전 시간
+    public float reloadTime = 2.0f;
+    //재장전 여부를 판단할 변수
+    private bool isReloading = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -62,9 +79,17 @@ public class FireCtrl : MonoBehaviour
     void Update()
     {
         //마우스 왼쪽 버튼을 클릭 했을 때 Fire 함수 호출
-        if(Input.GetMouseButtonDown(0))
+        if(!isReloading && Input.GetMouseButtonDown(0))
         {
+            //총알 수 하나 감소
+            --remainingBullet;
             Fire();
+
+            //남은 총알이 없을 경우 재장전 코루틴 호출
+            if(remainingBullet == 0)
+            {
+                StartCoroutine(Reloading());
+            }
         }
     }
 
@@ -83,6 +108,11 @@ public class FireCtrl : MonoBehaviour
 
         //사운드발생
         FireSfx();
+
+        //재장전 이미지의 fillAmount 속성값 지정
+        magazineImg.fillAmount = (float)remainingBullet / (float)maxBullet;
+        //남은 총알 수 갱신
+        UpdateBulletText();
     }
 
 
@@ -93,4 +123,28 @@ public class FireCtrl : MonoBehaviour
         //사운드 발생
         _audio.PlayOneShot(_sfx, 1.0f);
     }
+
+    IEnumerator Reloading()
+    {
+        isReloading = true;
+        _audio.PlayOneShot(playerSfx.reload[(int)currWeapon], 1.0f);
+
+        //재장전 오디오의 길이 + 0.3초 동안 대기
+        yield return new WaitForSeconds(playerSfx.reload[(int)currWeapon].length + 0.3f);
+
+        //각종 변수값의 초기화
+        isReloading = false;
+        magazineImg.fillAmount = 1.0f;
+        remainingBullet = maxBullet;
+        //남은 총알 수 갱신
+        UpdateBulletText();
+    }
+
+    void UpdateBulletText() 
+    {
+        //(남은 총알 수 / 최대 총알 수)  표시
+        magazineText.text = string.Format("<color=#ff0000>{0}</color>/{1}", remainingBullet, maxBullet);
+
+    }
+
 }
