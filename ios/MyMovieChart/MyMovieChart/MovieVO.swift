9acfa66ca404ba7f5d541e95843eec188eea41c4
sleepygloa <sleepygloa@gmail.com>
2022-04-04 16:46:58 +0900

    study ios tableview

---
+++ ios/MyMovieChart/MyMovieChart/MovieVO.swift
@@ -0,0 +1,17 @@
+//
+//  MovieVO.swift
+//  MyMovieChart
+//
+//  Created by seonho on 2022/04/02.
+//
+
+import Foundation
+
+class MovieVO{
+    var thumbnail: String? //영화, 썸네일 이미지 주소
+    var title: String? //영화 제목
+    var description: String? //영화 설명
+    var detail: String? //상세정보
+    var opendate: String? //개봉일
+    var rating: Double? //평점
+}
