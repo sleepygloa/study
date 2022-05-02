f4251790c0854e9ef78b0e4a0730462db90ab56a
sleepygloa <sleepygloa@gmail.com>
2022-05-02 23:45:44 +0900

    study ios custom button

--- ios/Chapter03-Alert/Chapter03-Alert/MapAlertViewController.swift
+++ ios/Chapter03-Alert/Chapter03-Alert/MapAlertViewController.swift
@@ -48,6 +48,20 @@ class MapAlertViewController : UIViewController{
         sliderBtn.addTarget(self, action: #selector(sliderAlert(_:)), for: .touchUpInside)
         
         self.view.addSubview(sliderBtn)
+        
+        //테이블 버튼
+        //버튼 생성
+        let listBtn = UIButton(type: .system)
+        
+        //버튼 속성 설정
+        listBtn.frame = CGRect(x: 0, y: 300, width: 100, height: 30)
+        listBtn.center.x = self.view.frame.width / 2
+        listBtn.setTitle("List Alert", for: .normal)
+        listBtn.addTarget(self, action: #selector(listAlert(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(listBtn)
+        
+        
     }
     
     @objc func mapAlert(_ sender:UIButton){
@@ -124,5 +138,27 @@ class MapAlertViewController : UIViewController{
         alert.setValue(contentVC, forKeyPath: "contentViewController")
         
         self.present(alert, animated: false)
+    }
+    
+    @objc func listAlert(_ sender:UIButton){
+        let contentVC = ListViewController()
+        
+        //델리게이트 객체를 자신으로 지정한다
+        contentVC.delegate = self
+        
+        //경고창 객체를 생성하고, OK 및 Cancel 버튼을 추가한다
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        
+        //뷰 컨트롤러를 알림창의 콘텐츠 뷰 컨트롤러 속성에 등록한다
+        alert.setValue(contentVC, forKeyPath: "contentViewController")
+        
+        self.present(alert, animated: false)
+    }
+    
+    func didSelectRowAt(indexPath: IndexPath){
+        print(">>>> 선택된 행은 \(indexPath.row) 입니다")
     }
 }
