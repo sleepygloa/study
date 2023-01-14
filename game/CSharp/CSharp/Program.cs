a4d304c36b6cbb7224264a5a5f15cca218d1f7fb
sleepygloa <sleepygloa@gmail.com>
2023-01-14 23:11:57 +0900

    C# 플레이어 생성

--- game/CSharp/CSharp/Program.cs
+++ game/CSharp/CSharp/Program.cs
@@ -12,6 +12,13 @@ namespace CSharp
             Mage = 3
         }
 
+        struct Player
+        {
+            public int hp;
+            public int attack;
+            //public ClassType type;
+        }
+
         //직업선택
         static ClassType ChooseClass() {
             Console.WriteLine("직업을 선택하세요");
@@ -27,14 +34,39 @@ namespace CSharp
                     choice = ClassType.Knight;
                     break;
                 case "2":
-                    choice = ClassType.Knight;
+                    choice = ClassType.Acher;
                     break;
                 case "3":
-                    choice = ClassType.Knight;
+                    choice = ClassType.Mage;
                     break;
             }
 
             return choice;
+        }
+
+        //캐릭터생성
+        //기사(100/10), 궁수(75/12), 법사(50/15)
+        static void CreatePlayer(ClassType choice, out Player player)
+        {
+            switch (choice)
+            {
+                case ClassType.Knight:
+                    player.hp = 100;
+                    player.attack = 10;
+                    break;
+                case ClassType.Acher:
+                    player.hp = 75;
+                    player.attack = 12;
+                    break;
+                case ClassType.Mage:
+                    player.hp = 50;
+                    player.attack = 15;
+                    break;
+                default:
+                    player.hp = 0;
+                    player.attack = 0;
+                    break;
+            }
         }
 
         static void Main(String[] args){
@@ -43,7 +75,15 @@ namespace CSharp
             {
                 ClassType choice = ChooseClass();
                 if (choice != ClassType.None)
-                    break;
+                {
+                    //캐릭터 생성
+                    Player player;
+                    CreatePlayer(choice, out player);
+
+                    Console.WriteLine($"HP{player.hp} Attack{player.attack}");
+
+                    //break;
+                }
             }
         }
     }
